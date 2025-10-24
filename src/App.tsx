0b6a2b72df25f36dabd104d3484a409ebe58e7ec
S0o0 <mezelhel@gmail.com>
2025-10-24 19:58:36 +0200

    ajout du logo signin

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Groups from './components/Groups';
 //import Teams from './components/Teams';
 const API_URL: string = import.meta.env.VITE_API_URL;
 import logo from './assets/wc26logo.avif'; // Logo CDM 2026
+import signin from './assets/usericon.png'; // Icone de connexion
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,17 +16,21 @@ function App() {
 
     <BrowserRouter>
       <header className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top">
-        <div className="container-fluid d-flex align-items-center">
-          <Link className="navbar-brand fw-bold d-flex align-items-center me-3" to="/">
+        <div className="d-flex align-items-center">
+          <Link className="navbar-brand fw-bold d-flex align-items-center me-3 ps-3" to="/">
             <img src={logo} alt="CDM 2026 Logo" style={{ height: '80px' }} />
           </Link>
+          <Link to="/" className="nav-link px-2 text-white">Accueil</Link>
+          <Link to="/matches" className="nav-link px-2 text-white">Matches</Link>
+          <Link to="/groups" className="nav-link px-2 text-white">Groupes</Link>
+          <Link to="/stadiums" className="nav-link px-2 text-white">Stades</Link>
+          <Link to="/teams" className="nav-link px-2 text-white">Équipes</Link>
+        </div>
 
-          <div className="d-flex align-items-center">
-            <Link to="/matches" className="nav-link px-2 text-white">Matches</Link>
-            <Link to="/groups" className="nav-link px-2 text-white">Groupes</Link>
-            <Link to="/stadiums" className="nav-link px-2 text-white">Stades</Link>
-            <Link to="/teams" className="nav-link px-2 text-white">Équipes</Link>
-          </div>
+        <div className="ms-auto">
+          <Link to="/auth" className="nav-link p-0 pe-3">
+            <img src={signin} alt="Sign in" style={{ height: '30px' }} />
+          </Link>
         </div>
       </header>
 
