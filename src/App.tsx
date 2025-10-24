58afc8348e353efe702649a3120a13e3ec217f5a
S0o0 <mezelhel@gmail.com>
2025-10-24 13:25:30 +0200

    ajout des variables d'environnement + import dans App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const API_URL: string = import.meta.env.VITE_API_URL;
 
 function App() {
   const [count, setCount] = useState(0)
