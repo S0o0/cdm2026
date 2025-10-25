d57238e21cfc318119e41f8161326cb8f7d7eed5
S0o0 <mezelhel@gmail.com>
2025-10-25 16:51:22 +0200

    ajout des images des stades

---
+++ src/components/StadiumPreview.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import type { Stadium } from '../types/Stadium';
+
+interface StadiumPreviewProps {
+    stadium: Stadium;
+}
+
+const StadiumPreview: React.FC<StadiumPreviewProps> = ({ stadium }) => {
+
+    return (
+        <div className="col-12 col-md-4 mb-4">
+            <div className="card text-white">
+                <div className="position-relative">
+                    <img 
+                        src={`https://worldcup2026.shrp.dev${stadium.imagePath}`} 
+                        className="card-img" 
+                        alt={stadium.name} 
+                    />
+                    <div className="card-img-overlay d-flex align-items-start p-2">
+                        <h5 className="card-title bg-dark bg-opacity-50 rounded px-2">{stadium.name}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default StadiumPreview;
