82f7c7eac0ef30036b4d911ce30d3822204c804e
S0o0 <mezelhel@gmail.com>
2025-10-25 17:26:11 +0200

    style stades

--- src/components/StadiumPreview.tsx
+++ src/components/StadiumPreview.tsx
@@ -8,17 +8,17 @@ interface StadiumPreviewProps {
 const StadiumPreview: React.FC<StadiumPreviewProps> = ({ stadium }) => {
 
     return (
-        <div className="col-12 col-md-4 mb-4 pt-4">
-            <div className="card text-white">
+        <div className="col-12 col-md-4 mb-4 pt-4 justify-content-center d-flex">
+            <div className="card text-white border-0 rounded">
                 <div className="position-relative">
                     <img 
                         src={`/stadiums/${stadium.id}.webp`} 
-                        className="card-img" 
+                        className="card-img rounded" 
                         alt={stadium.name} 
-                        style={{ width: '300px', height: '200px', objectFit: 'cover' }}
+                        style={{ width: '300px', height: '200px', objectFit: 'cover', filter: 'brightness(50%)' }}
                     />
-                    <div className="card-img-overlay d-flex align-items-start p-2">
-                        <h5 className="card-title bg-dark bg-opacity-50 rounded px-2">{stadium.name}</h5>
+                    <div className="card-img-overlay d-flex justify-content-start align-items-end p-2">
+                        <h5 className="card-title rounded px-3 fw-bold">{stadium.name}</h5>
                     </div>
                 </div>
             </div>
